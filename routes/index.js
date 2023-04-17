d6381d018c1ec6fb309611d1cdd09e748cd37344
Tyler J Richard <trichard668@gmail.com>
2023-04-17 01:25:21 -0700

    modified login page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/login', function(req, res, next) {
+  // retrieve login info via body parser
+  console.log(req.body.username+"-"+req.body.password)
+  res.redirect('/courses');
+});
+
 module.exports = router;
